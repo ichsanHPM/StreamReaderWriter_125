8a0e5266bb5c81bd997da7002af621e95475787c
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-06-19 11:37:23 +0700

    .

--- exceptionStandar/exceptionStandar.cpp
+++ exceptionStandar/exceptionStandar.cpp
@@ -10,7 +10,7 @@ int main()
     cout << "awal program" << endl; //penanda 1:...
     try {
         array < int, 3> data = { 1, 2, 3 };
-        //pesan array interger 3 element
+       
         //cout << data.at(5) << endl;
         //memanggil array element ke 5
     }
